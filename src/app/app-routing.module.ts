3e170ae017602be4fe227a5669de6a0220c4255a
Tushar <tushargulyani27@gmail.com>
2020-07-13 16:33:12 +0530

    added title and meta tags changed dynamically

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { Routes, RouterModule, Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { ContactComponent } from './contact/contact.component';
+import { Title, Meta } from '../../node_modules/@angular/platform-browser';
 
 
 const routes: Routes = [
   {
     path:'',
-    component: LandingpageComponent
-  }, 
+    component: LandingpageComponent,
+    data:{
+      title:"Home",
+      metaDescription: 'Clock time 1'
+    }
+  },                  
   {
     path:'contact',
-    component: ContactComponent
+    component: ContactComponent,
+    data:{
+      title:"Contact",
+      metaDescription: 'Clock time 2'
+    }
   }
 ];
 
@@ -19,4 +29,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  constructor(  ){  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { filter, map, mergeMap } from 'rxjs/operators';
+import { Router, ActivatedRoute, NavigationEnd } from '../../node_modules/@angular/router';
+import { Title, Meta } from '../../node_modules/@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortfolioApp';
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private router: Router, 
+    private titleService: Title,
+    private meta:Meta
+) { }
+
+ngOnInit() {
+// this.router
+// .events.pipe(
+// filter(event => event instanceof NavigationEnd),
+// map(() => {
+// let child = this.activatedRoute.firstChild;
+// while (child) {
+// if (child.firstChild) {
+//  child = child.firstChild;
+// } else if (child.snapshot.data && child.snapshot.data['title']) {
+//  return child.snapshot.data['title'];
+// } else {
+//  return null;
+// }
+// }
+// return null;
+// })).subscribe( (title: any) => {
+// this.titleService.setTitle(title);
+// });
+// }
+this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data))
+      .subscribe((event) => {
+        this.titleService.setTitle(event['title']);
+        console.log(event['title']);
+        console.log(event['metaDescription']);
+        var tag = { name: 'description', content: event['metaDescription'] };
+        this.meta.addTag(tag, false);
+      });
+}
 }
