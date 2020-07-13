81e67248110a390c3d91bec2299dbcbb68a0fd16
Tushar <tushargulyani27@gmail.com>
2020-07-13 11:31:30 +0530

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LandingpageComponent
+  }, 
+  {
+    path:'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
